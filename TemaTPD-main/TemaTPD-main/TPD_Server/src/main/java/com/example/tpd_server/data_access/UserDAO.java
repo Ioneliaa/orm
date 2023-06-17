c8d18a76a6c35a545b8251742ce48c9e20ecaeb4
Ioneliaa <122097741+Ioneliaa@users.noreply.github.com>
2023-06-17 18:17:48 +0300

    new artifact

--- TemaTPD-main/TemaTPD-main/TPD_Server/src/main/java/com/example/tpd_server/data_access/UserDAO.java
+++ TemaTPD-main/TemaTPD-main/TPD_Server/src/main/java/com/example/tpd_server/data_access/UserDAO.java
@@ -16,7 +16,7 @@ public final class UserDAO {
         ArrayList<User> result = new ArrayList<>();
 
         try (Connection conn = ConnectionHelper.getConnection();
-             PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM public.\"Users\"")) {
+             PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM public.\"User\"")) {
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
