809926ee8de0943fbb28428582968831b02f88e6
Ioneliaa <122097741+Ioneliaa@users.noreply.github.com>
2023-06-17 15:21:51 +0300

    update-connection-string

--- TemaTPD-main/TemaTPD-main/TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
+++ TemaTPD-main/TemaTPD-main/TPD_Server/src/main/java/com/example/tpd_server/data_access/ConnectionHelper.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionHelper {
-    private static final String url = "jdbc:postgresql://127.0.0.1:5432/postgres" ;
-    private static final String user="postgres";
-    private static final String password="postgres";
+    private static final String url = "jdbc:postgresql://34.116.161.183:5432/postgres?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=\t\n" +
+            "esoteric-pen-390014:europe-central2:postgres&user=postgres&password=postgres";
+
+
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(ConnectionHelper.url,ConnectionHelper.user,ConnectionHelper.password);
+            connection = DriverManager.getConnection(ConnectionHelper.url);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
